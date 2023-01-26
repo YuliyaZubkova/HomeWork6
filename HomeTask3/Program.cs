f9438fd8e7cef99e43002c1af0c0c300a100c691
Yuliya <i@yzubkova.ru>
2023-01-26 19:44:52 +0300

    Программа создает копию массива

--- HomeTask3/Program.cs
+++ HomeTask3/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
+//Инициализирует новый массив
+int[] InitArray(int length)
+{
+    int[] array = new int [length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(-9,10);
+    }
+
+    return array;
+}
+
+// Печатает массив на экран
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+
+// Копирует массив
+void ChangeElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine("");
+}
+
+int[] arr = InitArray(4);
+Console.Write("Введенный массив: ");
+PrintArray(arr);
+Console.Write("Копия массива: ");
+ChangeElements(arr);
