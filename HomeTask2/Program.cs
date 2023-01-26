7693a7400b5c0d1ea44a654716c33d8b8a6e92d7
Yuliya <i@yzubkova.ru>
2023-01-26 16:32:29 +0300

    Программа находит точку пересечения двух прямых, заданных уравнениями

--- HomeTask2/Program.cs
+++ HomeTask2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            isCorrect = true;
+        }
+        else 
+        {
+            Console.WriteLine("Ввели не число или 0ю Введите корректное число.");
+        }
+    }
+    return result;
+}
+
+double b1 = GetNumber("Введите b1: ");
+double k1 = GetNumber("Введите k1: ");
+double b2 = GetNumber("Введите b2: ");
+double k2 = GetNumber("Введите k2: ");
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+
