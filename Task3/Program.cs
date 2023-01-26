143d08551916eacd76e1f5ec9d657ad6b7b37e98
Yuliya <i@yzubkova.ru>
2023-01-26 15:17:51 +0300

    Программа переводит число из десятичной в двоичную систему

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая преобразовывает десятичное число в двоичное.
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+int fromBase = 10;
+int toBase = 2;
+
+string result = Convert.ToString(Convert.ToInt32(number, fromBase), toBase);
+
+Console.WriteLine($"Число {number} десятичной системы в двоичной системе будет числом: {result}");
+
