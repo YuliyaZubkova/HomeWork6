cd508ac1f26a947c3861e0c27406590a3e4883e3
Yuliya <i@yzubkova.ru>
2023-01-26 15:09:18 +0300

    Программа проверяет может ли существовать треугольник с заданными сторонами

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник такой длины.
+// Теорема: Каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne < numberTwo + numberThree && 
+    numberTwo < numberOne + numberThree &&
+    numberThree < numberOne + numberTwo) 
+    Console.WriteLine("Да");
+else Console.WriteLine("Нет");
