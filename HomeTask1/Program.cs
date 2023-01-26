04241eb4fb7de8d974d25de83bfd25e686738e47
Yuliya <i@yzubkova.ru>
2023-01-26 16:12:45 +0300

    Программа считает количество элеменов больше 0 среди введенных

---
+++ HomeTask1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Количество элементов > 0: {count}");
